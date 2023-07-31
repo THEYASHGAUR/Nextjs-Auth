0e407441f808295267b47860a3d7eaf895fd0901
Yash Gaur <90966838+THEYASHGAUR@users.noreply.github.com>
2023-07-31 17:30:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        MONGO_URI : "mongodb+srv://yash_gaur:auth1234@cluster0.ocuuksw.mongodb.net/"
+        MONGO_URI : "mongodb+srv://yash_gaur:***"""**@cluster0.ocuuksw.mongodb.net/"
     }
 }
 
